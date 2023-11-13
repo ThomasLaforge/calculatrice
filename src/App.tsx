9b5ef22603382a2028a5e41f20b8f66887479c46
ThomasLaforge <thomas.laforge.38@gmail.com>
2023-11-13 14:22:13 +0100

    correction de groupe

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React, { useCallback, useMemo, useState } from 'react';
 import './App.css';
 
-const calculatriceNumbers = new Array(10)
-  .fill("")
-  .map( (_,i) => i)
-  .reverse()
+let calculatriceNumbers: number[] = []
+for (let i = 0; i < 10; i++) {
+  calculatriceNumbers.push(i)
+}
 
 interface IOperator {
   symbol: string, 
@@ -33,11 +33,11 @@ const OPERATORS: IOperator[] = [
 function App() {
   const [numA, setNumA] = useState<number>(0)
   const [numB, setNumB] = useState<number>(0)
-  const [operator, setOperator] = useState<IOperator | null>(null)
+  const [operator, setOperator] = useState("")
 
   const handleNumberClick = useCallback(
     (num: number) => {
-      if(operator){
+      if(operator !== ""){
         setNumB(numB * 10 + num)
       }
       else {
@@ -48,49 +48,54 @@ function App() {
   )
 
   const handleOperatorClick = useCallback(
-    (o: IOperator) => {
-      if(numB || numB === 0){
-        if(operator){
-          setNumA(operator!.handler(numA, numB) )
+    (op: string) => {
+      if((numA || numA === 0) && (numB || numB === 0) && operator !== ""){
+        let result = 0
+        if(operator === "+"){
+          result = numA + numB
         }
-        else {
-          setOperator(o)
+        else if(operator === "-"){
+          result = numA - numB
         }
+
+        setNumA(result)
         setNumB(0)
       }
-      setOperator(o)
+      setOperator(op)
     },
     [operator, numA, numB]
   )
 
   const handleEqual = useCallback(
     () => {
-      if((numA || numA === 0) && (numB || numB === 0) && operator ){
-        setNumA(operator.handler(numA, numB))
+      if((numA || numA === 0) && (numB || numB === 0) && operator !== ""){
+        let result = 0
+        if(operator === "+"){
+          result = numA + numB
+        }
+        else if(operator === "-"){
+          result = numA - numB
+        }
+
+        setNumA(result)
         setNumB(0)
-        setOperator(null)
+        setOperator("")
       }
     }, [numA, numB, operator]
   )
-
-  const screenValue = useMemo(() => {
-    const numAValue = (numA || numA === 0) ? numA.toString() : ''
-    const numBValue = (numB) ? numB.toString() : ''
-    const operatorValue = operator?.symbol || ''
-    return `${numAValue} ${operatorValue} ${numBValue}` 
-  }, [numB, numA, operator])
 
   return (
     <div className="App">
       <h1>Calculatrice</h1>
       <div className="container">
         <div className="screen">
-          {screenValue}
+          {numA} {operator} {numB === 0 ? "" : numB}
         </div>
         <div className="buttons">
           <div className="numbers">
-            {calculatriceNumbers.map(num => 
+            {calculatriceNumbers.map( (num, index) => 
               <button 
+                key={index}
                 className="button-number"
                 onClick={() => handleNumberClick(num)}
               >
@@ -105,13 +110,18 @@ function App() {
               </button>
           </div>
           <div className="operators">
-            {OPERATORS.map(o => 
               <button
                 className="button-operator"
-                onClick={() => handleOperatorClick(o)}
+                onClick={() => handleOperatorClick("+")}
               >
-                {o.symbol}
-              </button>)}
+                +
+              </button>
+              <button
+                className="button-operator"
+                onClick={() => handleOperatorClick("-")}
+              >
+                -
+              </button>
           </div>
         </div>
       </div>
